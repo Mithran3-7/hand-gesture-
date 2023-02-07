9ec341e39ea9a0007a0a4c2daf271d10508df187
Mithran3-7 <112298923+Mithran3-7@users.noreply.github.com>
2023-02-07 16:47:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,4 +24,54 @@ function modelloaded(){
     var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterThis);
 
- }
+}
+function check(){
+   img=document.getElementById("captured_image");
+   classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+   if (error){
+console.error(error);
+   }
+   else{
+       console.log(results);
+       document.getElementById("result_emotion_name").innerHTML=results[0].label;
+
+       document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+
+       prediction_1=results[0].label;
+       prediction_2=results[1].label;
+       speak();
+       if(results[0].label=="amazing")
+       {
+           document.getElementById("update_emoji").innerHTML="👌";
+       } 
+
+       if(results[0].label=="peace")
+       {
+           document.getElementById("update_emoji").innerHTML="✌️";
+       } 
+
+       if(results[0].label=="ok")
+       {
+           document.getElementById("update_emoji").innerHTML="👍";
+       } 
+
+
+       if(results[1].label=="amazing")
+       {
+           document.getElementById("update_emoji2").innerHTML="👌";
+       } 
+
+       if(results[1].label=="peace")
+       {
+           document.getElementById("update_emoji2").innerHTML="✌️";
+       } 
+
+       if(results[1].label=="ok")
+       {
+           document.getElementById("update_emoji2").innerHTML="👍";
+       } 
+   }
+   
+}
